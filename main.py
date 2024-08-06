cb0d496ecb12a2239299eab73b4585f329e6c950
Chukwukwadorom <chikwadonwattah@gmail.com>
2024-08-07 00:46:05 +0100

    added data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.kidney_disease_classifier import logger
+from kidney_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to testing logging")
+stage_name = "Data ingestion stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {stage_name} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
--- src/kidney_disease_classifier/utils/common.py
+++ src/kidney_disease_classifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
+from kidney_disease_classifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
